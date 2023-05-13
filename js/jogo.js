f39738efe5af7a3351ec80b67a719975b8cb4b07
Artur Matos Malcher de Sousa <128944828+ArturMalcher@users.noreply.github.com>
2023-05-13 15:02:50 -0300

    commit inicial
    Um simples jogos da forca

---
+++ js/jogo.js
@@ -0,0 +1,54 @@
+var count = 0;
+var a = false;
+var b = false;
+var c = false;
+var d = false;
+
+function erro() {
+if (count < 6) {
+count = count + 1
+document.getElementById("forca").src ="images/"+count+".png";
+  }
+}
+
+function win() {
+  if (a && b && c && d) {
+    alert("VOCÊ VENCEU, PARABÉNS!");
+    location.reload();
+  }
+}
+
+document.addEventListener('keypress',(event) => {
+ var name = event.key;
+ var code = event.code;
+ 
+ if (count == 6) {
+    alert("VOCÊ PERDEU, DESISTA!");
+    location.reload();
+  }
+  
+ if ( name == "j" || (name == "J")){
+    document.getElementById("a").innerHTML="J";
+    a = true;
+    win();
+ }
+  else if ( name == "o" || (name == "O")){
+    document.getElementById("b").innerHTML="O";
+    b = true;
+    win();
+ }
+  else if ( name == "a" || (name == "A")){
+    document.getElementById("c").innerHTML="A";
+    document.getElementById("f").innerHTML="A";
+    c = true;
+    win();
+ }
+  else if ( name == "n" || (name == "N")){
+    document.getElementById("d").innerHTML="N";
+    document.getElementById("e").innerHTML="N";
+    d = true;
+    win();
+ } else {
+  erro();
+  }
+}, false);
